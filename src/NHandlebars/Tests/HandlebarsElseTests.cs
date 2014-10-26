d6aa880e6804d97ed216c6e4d6d52af8106cec8f
sergeyt <stodyshev@gmail.com>
2014-10-26 10:31:24 +0600

    tests (#4)

---
+++ src/NHandlebars/Tests/HandlebarsElseTests.cs
@@ -0,0 +1,64 @@
+﻿#if NUNIT
+using NUnit.Framework;
+
+namespace NHandlebars.Tests
+{
+	/// <summary>
+	/// From https://github.com/sergeyt/NHandlebars/issues/4
+	/// </summary>
+	[TestFixture]
+	public class HandlebarsElseTests
+	{
+		private const string Template = "{{#each County}}" +
+											"{{#if @first}}" +
+												"{{this}}" +
+											"{{else}}" +
+												"{{#if @last}}" +
+													" and {{this}}" +
+												"{{else}}" +
+													", {{this}}" +
+												"{{/if}}" +
+											"{{/if}}" +
+										"{{/each}}";
+
+		[Test]
+		public void CountyHasOneValue()
+		{
+			var options = new
+			{
+				County = new[] { "Kane" }
+			};
+
+			var actual = Handlebars.Render(Template, options);
+
+			Assert.That(actual, Is.EqualTo("Kane"));
+		}
+
+		[Test]
+		public void CountyHasTwoValue()
+		{
+			var options = new
+			{
+				County = new[] { "Kane", "Salt Lake" }
+			};
+
+			var actual = Handlebars.Render(Template, options);
+
+			Assert.That(actual, Is.EqualTo("Kane and Salt Lake"));
+		}
+
+		[Test]
+		public void CountyHasMoreThanTwoValue()
+		{
+			var options = new
+			{
+				County = new[] { "Kane", "Salt Lake", "Weber" }
+			};
+
+			var actual = Handlebars.Render(Template, options);
+
+			Assert.That(actual, Is.EqualTo("Kane, Salt Lake and Weber"));
+		}
+	}
+}
+#endif
